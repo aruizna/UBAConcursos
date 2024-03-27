2b40623b5dda14a477cfb5f82da3c422e42518b2
aruizna <aruizna@bd360.online>
2024-03-27 17:48:39 -0300

    change in docker and default values

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' =>  'mysql:host=' . ($_ENV['MYSQL_HOST'] ?? "localhost") . ':' . ($_ENV['MYSQL_PORT'] ?? 3306) . ';dbname=' . ($_ENV['MYSQL_DATABASE'] ?? "UBA_Concursos"),
-    'username' => $_ENV['MYSQL_USER'] ?? 'admin',
-    'password' => $_ENV['MYSQL_PASSWORD'] ?? 'adminadmin',
+    'dsn' =>  'mysql:host=' . ($_ENV['MYSQL_HOST'] ?? "db") . ':' . ($_ENV['MYSQL_PORT'] ?? 3306) . ';dbname=' . ($_ENV['MYSQL_DATABASE'] ?? "oncursos"),
+    'username' => $_ENV['MYSQL_USER'],
+    'password' => $_ENV['MYSQL_PASSWORD'],
     'charset' => 'utf8',
 ];
