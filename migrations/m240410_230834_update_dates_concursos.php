43435ed7c651bf5732a61ff3e65c29fdd23719b3
Héctor Ricardo Domínguez <hrdominguez@ambiente.gob.ar>
2024-04-10 20:26:48 -0300

    se suben cambios de fecha y correccion de horarios a concursos

---
+++ migrations/m240410_230834_update_dates_concursos.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240410_230834_update_dates_concursos
+ */
+class m240410_230834_update_dates_concursos extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-04652968-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-05003399-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-05018619-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-05164911-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-03623530-   -UBA-DIMEDA#SA_FI"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-05213279-   -UBA-DMEDA#FODON"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-11-23 09:00:00", "fecha_fin_inscripcion"=>"2024-04-15 18:00:00"], ["numero_expediente" => "EX-2023-05300780-   -UBA-DMEDA#FODON"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-02397009-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05004502-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05004557-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05018503-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05003285-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05003795-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05004215-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05138214-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05138264-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05164318-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05233079-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05254860-   -UBA-DMESA#FCEN"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05465307-   -UBA-DMESA#SG_FVET"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05465416-   -UBA-DMESA#SG_FVET"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05465474-   -UBA-DMESA#SG_FVET"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05465535-   -UBA-DMESA#SG_FVET"]);
+    $this->update("concurso",["fecha_inicio_inscripcion"=>"2023-12-12 09:00:00", "fecha_fin_inscripcion"=>"2024-05-02 18:00:00"], ["numero_expediente" => "EX-2023-05121189-   -UBA-DMEDA#FODON"]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240410_230834_update_dates_concursos cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m240410_230834_update_dates_concursos cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
