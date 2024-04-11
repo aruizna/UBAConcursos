da82827031bee4bb841febc8c09b8733ab67962b
Héctor Ricardo Domínguez <hrdominguez@ambiente.gob.ar>
2024-04-10 21:45:07 -0300

    se corrige error de query que elimina usuarios que ocupan cargos en concursos

--- migrations/m240410_234557_delete_users_who_hold_positions.php
+++ migrations/m240410_234557_delete_users_who_hold_positions.php
@@ -12,13 +12,15 @@ class m240410_234557_delete_users_who_hold_positions extends Migration
      */
     public function safeUp()
     {
-        $this->execute("DELETE FROM user
-        WHERE id IN (
-            SELECT u.id
-            FROM user AS u
-            INNER JOIN profile AS p ON u.id = p.user_id
-            INNER JOIN persona_concurso_renovacion AS pcr ON p.numero_documento = pcr.numero_documento
-        )");
+        $sql = "DELETE FROM user WHERE id IN (
+            SELECT * FROM (
+                SELECT u.id
+                FROM user AS u
+                INNER JOIN profile AS p ON u.id = p.user_id
+                INNER JOIN persona_concurso_renovacion AS pcr ON p.numero_documento = pcr.numero_documento
+            ) AS subquery
+        )";
+        Yii::$app->db->createCommand($sql)->execute();
     }
 
     /**
