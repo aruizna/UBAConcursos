d6c002ad2a770ba6c13be804c30418135b503c02
Angeles <apena@grupomercel.com>
2024-05-09 18:23:38 -0300

    update login and register with new responsive styles

--- deploy/vendor/2amigos/yii2-usuario/src/User/resources/views/security/login.php
+++ deploy/vendor/2amigos/yii2-usuario/src/User/resources/views/security/login.php
@@ -24,55 +24,164 @@ use yii\widgets\MaskedInput;
 ?>
 
 <?= $this->render('/shared/_alert', ['module' => Yii::$app->getModule('user')]) ?>
+<link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
 <style>
-    main{
-        background-color:#EEE8E7;
-        background-repeat: no-repeat;
-        background-size: cover;
-        width:100%;
-        height:650px;
+body {
+    font-family: "Bitter", serif;
+    font-weight: 400;
+    height: 100vh;
+    font-size: 18px;
+    color: #1d2554;
+    margin: 0;
+    padding: 0;
+}
+
+main {
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h1 {
+    color: white;
+    font-size: 30px
+}
+
+h3 {
+    color: white;
+    font-size: 20px
+}
+
+.invalid-feedback {
+    font-size: 10px
+}
+
+.display {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    flex-wrap: wrap;
+    border: solid #bbbbbb 1px
+}
+
+.segmento {
+    display: flex;
+    flex-direction: column;
+    min-width: 330px;
+    min-height: 260px;
+    padding: 2vh;
+    box-sizing: border-box;
+    justify-content: space-around;
+    align-items: center
+}
+
+@media screen and (max-width: 993px) {
+    .display {
+
+        border: none
     }
-    .triangle {
-        width: 0;
-        height: 0;
-        border-top: 650px solid transparent;
-        border-left: 1100px solid white;
-        position: absolute;
-        left: 0;
+
+    .segmento {
+        border: solid #bbbbbb 1px
     }
-    /* Chrome, Safari, Edge, Opera */
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
+}
+
+
+.link-registrarse {
+    font-size: 16px;
+    text-decoration: none;
+    font-weight: 200;
+    color: #fccd8e
+}
+
+.link-registrarse:hover {
+    color: #91bde1
+}
+
+
+
+.link-registrarse b {
+    font-weight: 600;
+}
+
+.link-contraseña {
+    display: flex;
+    font-size: 16px;
+    align-self: center;
+    text-decoration: none;
+    font-weight: 600;
+    color: #91bde1
+}
+
+.link-contraseña:hover {
+    color: #fccd8e
+}
+
+.button-siguiente {
+    background-color: #f7a600;
+    margin: 3vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: #FFFFFF;
+    padding: 10px 20px;
+    transition: background-color 0.3s, color 0.3s;
+    width: 200px;
+    border: none;
+}
+
+.button-siguiente:hover {
+    background-color: #91bde1;
+}
+
+.input-texto {
+    border-radius: 0;
+    background-color: #bbbbbb20;
+    max-width: 350px;
+    text-align: center;
+    font-size: 14px;
+}
+
+.text-center {
+    text-align: center;
+}
+
+a {
+    text-decoration: none;
+    font-weight: 200;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
-    }
+}
 
-    /* Firefox */
-    input[type=number] {
+/* Firefox */
+input[type=number] {
     -moz-appearance: textfield;
-    }
-    .card{
-        border:none;
-    }
+}
 </style>
-<div class="triangle"></div>
 
-<div class="row" style="margin-top:100px;">
-    <div class="col" style="justify-content:center">
-        <div class="row" style="max-width:fit-content;margin: 0 auto;">
-            <div class="col mx-0 px-0" style="border:transparent">
-                <div class="card" style="justify-content:center;align-items:center;border-radius:0;background-color:rgba(56, 56, 56, 0.1);width:500px;height:300px;width:400px">
-                    <H1>¡Bienvenido!</H1>
-                    <H3 style="font-weight: 400;">Iniciar Sesión</H3>
-                    <p class="text-center" style="position:absolute;bottom:5px">
-                    <a href="<?=Url::to(['/user/registration/register'])?>" style="text-decoration: none;color:black;font-weight: 200;font-family: Helvetica;">¿No está registrado? <b style="font-weight: 600;font-family: Helvetica;">REGISTRARSE</b></a>
-                    </p>
-                </div>
-            </div>
-            <div class="col mx-0 px-0" style="border:transparent;">
-                <div class="card" style="border-radius:0;height:300px;width:400px">
-                    <div class="card-body">
-                        <?php $form = ActiveForm::begin(
+<div class="display">
+    <div class="segmento" style="background-color: #1d2554">
+        <H1>¡Bienvenido!</H1>
+        <H3 style="font-weight: 400;">Iniciar Sesión</H3>
+        <p class="text-center">
+            <a class="link-registrarse" href="<?=Url::to(['/user/registration/register'])?>">¿No está
+                registrado?
+                <b>REGISTRARSE</b></a>
+        </p>
+
+    </div>
+    <div class="segmento">
+        <?php $form = ActiveForm::begin(
                             [
                                 'id' => $model->formName(),
                                 'enableAjaxValidation' => true,
@@ -80,12 +189,11 @@ use yii\widgets\MaskedInput;
                                 'validateOnBlur' => false,
                                 'validateOnType' => false,
                                 'validateOnChange' => false,
-                                'options' => ['style' => 'display: flex;flex-direction:column;height: 100%;justify-content: space-around;'],
+                                'options' => ['class' => 'd-flex flex-column justify-content-around h-100'],
                             ]
                         ) ?>
-                        <div style="display:flex;flex-direction:column;align-items: center;justify-content: center;">
-                        
-                            <!-- <div style="display:flex;flex-direction:row-reverse;margin-bottom: 20px;">
+
+        <!-- <div style="display:flex;flex-direction:row-reverse;margin-bottom: 20px;">
                                 <select name="documento" class="text-center" id="doctipo" style="position: relative;bottom:auto;height:30px;width:120px;border:1px solid black;border-radius:10px;background-color:transparent">
                                     <option value="dni">DNI</option>
                                     <option value="CUIL">CUIL</option>
@@ -93,43 +201,41 @@ use yii\widgets\MaskedInput;
                                 <p style="margin:0;margin-right:10px;font-weight:500">Tipo de Documento</p>
                             </div> -->
 
-                            <?= $form->field(
+        <?= $form->field(
                                 $model,
                                 'login',
-                                ['inputOptions' => ['id' => 'usernameinput', 'style' => 'border-radius:10px;background-color:transparent;width:250px;border:1px solid black', 'autofocus' => 'autofocus', 'class' => 'text-center form-control', 'tabindex' => '1', 'placeholder' => 'Nº de documento']]
+                                ['inputOptions' => ['id' => 'usernameinput', 'autofocus' => 'autofocus', 'class' => 'input-texto form-control', 'tabindex' => '1', 'placeholder' => 'Nº de documento']]
                             )->label(false)->textInput(['type' => 'number']); ?>
 
-                            <?= $form
+        <?= $form
                                 ->field(
                                     $model,
                                     'password',
-                                    ['inputOptions' => ['style' => 'border-radius:10px;background-color:transparent;width:250px;border:1px solid black','class' => 'text-center form-control', 'tabindex' => '2', 'placeholder' => 'Contraseña']]
+                                    ['inputOptions' => ['class' => 'input-texto form-control', 'tabindex' => '2', 'placeholder' => 'Contraseña']]
                                 )
                                 ->passwordInput()->label(false)  ?>
 
-                            <div style="justify-content: space-around;display: flex;">
-                                <?= Html::submitButton(
-                                    Yii::t('usuario', 'Siguiente'),
-                                    ['class' => 'btn btn-block', 'style'=>'width:150px;background-color:#40BB97;font-weight:600','tabindex' => '3']
+        <?= Html::submitButton(
+                                    Yii::t('usuario', 'SIGUIENTE'),
+                                    ['class' => 'button-siguiente', 'tabindex' => '3']
                                 ) ?>
-                            </div>
-                            <a href="<?=Url::to(['/user/forgot'])?>" class="mt-3" style="text-decoration: none;color:black;font-weight: 200;font-family: Helvetica;">¿Olvidó su contraseña?</a>
+        <a href="<?=Url::to(['/user/forgot'])?>" class="link-contraseña">¿Olvidó su
+            contraseña?</a>
 
-                        </div>
 
-                        <?php ActiveForm::end(); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
 
-        <?= ConnectWidget::widget(
+</div>
+<?php ActiveForm::end(); ?>
+
+
+
+<?= ConnectWidget::widget(
             [
                 'baseAuthUrl' => ['/user/security/auth'],
             ]
         ) ?>
-    </div>
-</div>
+
 
 
 <?php
--- views/site/index.php
+++ views/site/index.php
@@ -8,161 +8,210 @@ use yii\helpers\Url;
 
 $this->title = 'Concursos';
 ?>
-<link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
 <style>
-    body {
-        font-family: "Bitter", serif;
-        font-weight: 400;
-        font-size: 18px;
-        color: #1d2554;
-        margin: 0;
-        padding: 0;
-    }
-    .button-line{
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        margin-bottom: 30px;
-        margin-top: 10px
-    }
-   
-    .authbutton {
-        background-color: #f7a600;
-        margin: 5px;
-        display: flex;
-        justify-content: center;
+body {
+    font-family: "Bitter", serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: #1d2554;
+    margin: 0;
+    padding: 0;
+}
+
+.button-line {
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0px;
+    margin-bottom: 30px;
+    width: 100%;
+}
+
+.authbutton {
+    background-color: #f7a600;
+    margin: 0 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: #FFFFFF;
+    padding: 10px 20px;
+    transition: background-color 0.3s, color 0.3s;
+    width: 200px;
+
+}
+
+.button-group {
+    display: flex;
+    gap: 10px;
+    position: absolute;
+    right: 0;
+    /* Justifica el grupo de botones al borde derecho */
+}
+
+
+.title {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    font-size: 40px;
+    font-weight: 900;
+    color: #1d2554;
+    margin: 0;
+    white-space: nowrap;
+}
+
+.authbutton:hover {
+    background-color: #91bde1;
+}
+
+@media screen and (max-width: 1200px) and (min-width: 769px) {
+    .button-line {
+        flex-direction: column;
+        /* Coloca los botones debajo del título */
         align-items: center;
-        text-decoration: none;
-        color: #FFFFFF;
-        padding: 10px 20px;
-        transition: background-color 0.3s, color 0.3s;
-        width: 45%; 
-        max-width: 240px;
     }
 
-    .authbutton:hover {
-        background-color: #91bde1;
+    .title {
+        position: static;
+        transform: none;
+        margin: 10px 0;
     }
 
-    @media screen and (max-width: 768px) {
+    .button-group {
+        position: static;
+        flex-direction: row;
+        gap: 10px;
+        margin-top: 10px;
+    }
+}
+
+@media screen and (max-width: 768px) {
     .button-line {
         flex-direction: column;
         align-items: center;
-        
+
+    }
+
+    .button-group {
+        position: static;
+        /* Cambia la posición a estática para pantallas pequeñas */
+        margin: 5px 0;
+        flex-direction: column;
     }
 
     .authbutton {
-        width: calc(100% - 30px);
+        max-width: 400px width: calc(100% - 30px);
         margin: 5px 0;
     }
 }
 
-    .title {
-        font-size: 40px;
-        font-weight: 900;
-        color: #1d2554;
-    }
+.title {
+    position: static;
+    transform: none;
+    margin: 10px 0;
+}
 
-    .text {
-        color: #1d2554;
-    }
 
-    .card {
-        border: none;
-        background-color: transparent;
-        width: 100%;
-        max-width: 600px;
-        margin: 0 auto;
-        text-align: center;
-    }
+.button-container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
 
+.bigButton {
+    cursor: pointer;
+    background-color: #bdd7ed;
+    width: calc(50% - 20px);
+    height: 27vh;
+    margin: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 26px;
+    text-align: center;
+    padding: 10px;
+    font-weight: 600
+}
+
+.bigButton2 {
+    background-color: #fccd8e
+}
+
+@media screen and (max-width: 768px) {
     .button-container {
-        width: 80%; 
-        margin: 0 auto;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
+        width: 100%;
+        margin: 0;
     }
+
     .bigButton {
-        cursor: pointer;
-        background-color: #bdd7ed;
-        width: calc(50% - 20px); 
-        height: 214px;
-        margin: 5px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 36px;
-        text-align: center; 
-        padding: 10px;
-        font-weight: 600
-        
+        width: calc(100% - 20px);
+        font-size: 24px;
+        height: 100px;
+        margin: 10px 0;
     }
-    .bigButton2 {
-        background-color: #fccd8e
-    }
-    @media screen and (max-width: 768px) {
-        .button-container {
-            width: 100%; /* Ancho completo en dispositivos pequeños */
-            margin: 0; /* Eliminar el margen lateral en dispositivos pequeños */
-        }
-        .bigButton {
-            width: calc(100% - 20px); /* Ancho completo para una columna con margen */
-            font-size: 24px; /* Reducir el tamaño de la fuente para dispositivos pequeños */
-            height: 100px; /* Altura reducida para dispositivos pequeños */
-            margin: 10px 0; /* Aumentar el margen vertical entre los botones en dispositivos pequeños */
-        }
-    }
+}
 
-    .announcement {
+.announcement {
 
-        margin: 20px auto;
-        padding: 10px;
-        background-color: #1d2554;
-        color: #FFFFFF;
-        text-align: center;
-        width: 100%
-    }
+    margin: 20px auto;
+    padding: 10px;
+    background-color: #1d2554;
+    color: #FFFFFF;
+    text-align: center;
+    width: 100%
+}
 </style>
 
 <div class="site-index">
     <?php if (Yii::$app->user->isGuest) : ?>
 
-        <div class="card">
+    <div>
+
+        <div class="button-line">
             <h2 class="title">Bienvenido</h2>
-            <div class="button-line">
+            <div class="button-group">
                 <a href="<?= Url::to(['/user/security/login']) ?>" class="authbutton">INICIAR SESIÓN</a>
                 <a href="<?= Url::to(['/user/registration/register']) ?>" class="authbutton">REGISTRARSE</a>
             </div>
         </div>
+    </div>
 
     <?php endif ?>
     <?php if (!Yii::$app->user->isGuest) : ?>
-        <div class="warning" >
-            <p class="text"><b>Importante:</b> antes de iniciar una preinscripción a un concurso, debe ingresar en la sección Mi Perfil a través del Menú que se encuentra arriba a la derecha y completar todos los datos obligatorios</p>
-        </div>
+    <div class="warning">
+        <p class="text"><b>Importante:</b> antes de iniciar una preinscripción a un concurso, debe ingresar en la
+            sección Mi Perfil a través del Menú que se encuentra arriba a la derecha y completar todos los datos
+            obligatorios</p>
+    </div>
     <?php endif ?>
     <div class="button-container">
-            <div class="bigButton" style="" onclick="location.href='<?= Url::to(['/concurso']) ?>'">
-                <p class="text">Llamados a concursos</p>
-           
-            </div>
-            <div class="bigButton bigButton2"  onclick="location.href='<?= Url::to(['/site/datos-utiles']) ?>'">
-                <p class="text">Datos útiles de las UUAA</p>
-            </div>
+        <div class="bigButton" style="" onclick="location.href='<?= Url::to(['/concurso']) ?>'">
+            <p class="text">Llamados a concursos</p>
 
-            <div class="bigButton bigButton2"  onclick="location.href='<?= Url::to(['/concurso/tramite']) ?>'">
-                <p class="text">Mis concursos en trámite</p>
-            </div>
-            <div class="bigButton" onclick="location.href='<?= Url::to(['documentos']) ?>'">
-                <p class="text">Documentación a presentar</p>
-            </div>
-      
+        </div>
+        <div class="bigButton bigButton2" onclick="location.href='<?= Url::to(['/site/datos-utiles']) ?>'">
+            <p class="text">Datos útiles de las UUAA</p>
+        </div>
+
+        <div class="bigButton bigButton2" onclick="location.href='<?= Url::to(['/concurso/tramite']) ?>'">
+            <p class="text">Mis concursos en trámite</p>
+        </div>
+        <div class="bigButton" onclick="location.href='<?= Url::to(['documentos']) ?>'">
+            <p class="text">Documentación a presentar</p>
+        </div>
+
     </div>
     <?php if (Yii::$app->user->isGuest) : ?>
-        <div class="announcement">
-            <p class="textWhite"><b>Importante:</b> Para poder preinscribirse en un concurso publicado por la Universidad de Buenos Aires, es necesario que sea un usuario registrado.</p>
-        </div>
+    <div class="announcement">
+        <p class="textWhite"><b>Importante:</b> Para poder preinscribirse en un concurso publicado por la Universidad de
+            Buenos Aires, es necesario que sea un usuario registrado.</p>
+    </div>
     <?php endif ?>
 </div>
