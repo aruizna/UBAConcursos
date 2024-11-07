abe28a004c600eede0aa4dad14f8cae2091dfed7
juanocordoba <juancordoba75@gmail.com>
2024-11-07 12:58:04 -0300

    fix carga 20241104

---
+++ migrations/m241107_152844_fix_create_nuevos_concursos_20241104.php
@@ -0,0 +1,248 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241107_152844_fix_create_nuevos_concursos_20241104
+ */
+class m241107_152844_fix_create_nuevos_concursos_20241104 extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+    
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-02833286-   -UBA-DMESA#SSA_FAGRO"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-04552534-   -UBA-DMESA#SSA_FAGRO"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-04552639-   -UBA-DMESA#SSA_FAGRO"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-04552734-   -UBA-DMESA#SSA_FAGRO"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-04821777-   -UBA-DMESA#SSA_FAGRO"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-03950321-   -UBA-DMESA#FCEN"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-03986929-   -UBA-DMESA#FCEN"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-04931090-   -UBA-DMESA#FCEN"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-04931128-   -UBA-DMESA#FCEN"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-02546494-   -UBA-DIMEDA#SA_FI"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-02546644-   -UBA-DIMEDA#SA_FI"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-02575594-   -UBA-DIMEDA#SA_FI"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05077894-   -UBA-DMEA#FMED"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05077942-   -UBA-DMEA#FMED"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05180858- -UBA-DMEDA#FODON"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05025049-   -UBA-DMRD#SHA_FPSI"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05025072-   -UBA-DMRD#SHA_FPSI"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05025101-   -UBA-DMRD#SHA_FPSI"]);
+        $this->update("concurso", ["fecha_fin_inscripcion" => "2024-12-05 18:00:00", "fecha_inicio_inscripcion" => "2024-11-07 09:00:00", "fecha_publicacion" => "2024-11-06"], ["numero_expediente" => "EX-2024-05025119-   -UBA-DMRD#SHA_FPSI"]);
+
+        $sql = "
+            DELETE FROM concurso_asignatura
+            WHERE id_concurso IN (
+                SELECT id_concurso
+                FROM concurso
+                WHERE numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+            )
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        # EX-2024-04931090-   -UBA-DMESA#FCEN	- 4
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            930,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2729,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+        
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            198,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+    
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2732,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+
+        Yii::$app->db->createCommand($sql)->execute();
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2730,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2731,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2754,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+        
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            1754,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+    
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2759,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+
+        Yii::$app->db->createCommand($sql)->execute();
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2761,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+
+
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2753,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2755,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2746,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2757,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+        
+        $sql = "
+            insert INTO concurso_asignatura
+            select
+            c.id_concurso,
+            2577,
+            4
+            from concurso c
+            where c.numero_expediente = 'EX-2024-04931090-   -UBA-DMESA#FCEN'
+        ";
+        Yii::$app->db->createCommand($sql)->execute();
+
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241107_152844_fix_create_nuevos_concursos_20241104 cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m241107_152844_fix_create_nuevos_concursos_20241104 cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
